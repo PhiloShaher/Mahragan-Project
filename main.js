16fed132b865e18ac87a139b545e3daea09be7f4
PhiloShaher <455546450@cairo1.moe.edu.eg>
2025-08-25 21:57:57 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  // Load the church.html file.
+  win.loadFile('church.html');
+
+  // Optional: Open the DevTools.
+  // win.webContents.openDevTools();
+}
+
+// This method will be called when Electron has finished initialization.
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// Quit when all windows are closed, except on macOS.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
